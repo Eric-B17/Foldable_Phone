d7d98d06277c913c066c664de809085a46c2e19c
Bishoeri000 <Bishoeri000@warren.k12.in.us>
2024-09-23 12:24:26 -0400

    Initial

---
+++ src/Main.kt
@@ -0,0 +1,35 @@
+class Phone(var isScreenLightOn: Boolean = false) {
+    fun switchOn() {
+        isScreenLightOn = true
+    }
+
+    fun switchOff() {
+        isScreenLightOn = false
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(var isScreenFolded: Boolean = false) {
+    fun switchOn() {
+        isScreenFolded = false
+    }
+
+    fun switchOff() {
+        isScreenFolded = true
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreen = if (isScreenFolded) "on because its not folded" else "off because it is folded"
+        println("The phone screen is $phoneScreen.")
+    }
+}
+
+
+fun main() {
+    val phone = FoldablePhone()
+    phone.checkPhoneScreenLight()
+}
